392132e5db65a2fd433072d4094ee8de7df03639
Mai Le <s86123@bht-berlin.de>
2023-10-05 15:01:01 +0200

    lession 1

---
+++ area_calc.py
@@ -0,0 +1,9 @@
+# name = input('Tell me your name: ')
+# age = input('Tell me your age: ')
+# print(name, 'you are', age)
+
+# Calc the area of a circle
+r = input('Enter the radius of your circle (m): ')
+area = 3.142 * int(r)**2
+print('The area of your circle is:', area)
+
---
+++ string_formatting.py
@@ -0,0 +1,12 @@
+# String formatting
+num1 = 3.13324453
+num2 = 10.243434
+
+# Previous formatting
+#print('num 1 is', num1, 'and num 2 is', num2)
+
+# Format method
+#print('num 1 is {0:.3f} and num 2 is {1:.3f}'.format(num1, num2))
+
+# Using f-strings
+print(f'num 1 is {num1:.2f} num 2 is {num2:.2f}')
